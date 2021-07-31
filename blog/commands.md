f249afc0b35166c244d4a9d0feb1e81972ee668c
Daniel Tomlinson <dtomlinson@panaetius.co.uk>
2021-07-31 05:44:39 +0100

    adding latest

--- blog/commands.md
+++ blog/commands.md
@@ -3,3 +3,9 @@
 ## Convert svg to png
 
 `rsvg-convert -h 1000 banner.svg > banner.png`
+
+## Docker
+
+### Algolia
+
+`docker run -it --env-file=.env -e "CONFIG=$(cat /Users/dtomlinson/git-repos/web-dev/panaetius-blog/blog/docsearch.json | jq -r tostring)" algolia/docsearch-scraper`
--- blog/gulpfile.js/index.js
+++ blog/gulpfile.js/index.js
@@ -1,29 +1,29 @@
 // Imports
-const { run } = require("./build-lunrjs-index");
+// const { run } = require("./build-lunrjs-index");
 const gulp = require("gulp");
 const util = require("util");
 const execFile = util.promisify(require("child_process").execFile);
 const exec = util.promisify(require("child_process").exec);
 const { getCurrentDir } = require("./utils");
-const fs = require("fs");
+// const fs = require("fs");
 const imagemin = require("gulp-imagemin");
-const uglify = require("gulp-uglify");
-const hash = require("gulp-hash-filename");
-const rename = require("gulp-rename");
+// const uglify = require("gulp-uglify");
+// const hash = require("gulp-hash-filename");
+// const rename = require("gulp-rename");
 const clean = require("gulp-clean");
-const replace = require("gulp-replace");
-const merge = require("merge-stream");
+// const replace = require("gulp-replace");
+// const merge = require("merge-stream");
 
 // Directories
 const currentDir = getCurrentDir(__dirname);
 const themeDir = getCurrentDir(__dirname) + "/themes/panaetius-theme";
 
 // Function to build and write the search index
-async function buildSearch(cb) {
-  let searchIndex = await run();
-  fs.writeFileSync(currentDir + "/static/search-index.json", searchIndex);
-  cb();
-}
+// async function buildSearch(cb) {
+//   let searchIndex = await run();
+//   fs.writeFileSync(currentDir + "/static/search-index.json", searchIndex);
+//   cb();
+// }
 
 // Function to build the Hugo project
 async function buildHugo(cb) {
@@ -62,66 +62,65 @@ async function syncAssetsR(cb) {
 async function buildTheme(cb) {
   console.log(themeDir);
   await exec(`cd ${themeDir} && yarn buildGlobal `);
-  // await exec(`cd ${themeDir} && yarn buildPosts`);
   cb();
 }
 
 // Clear ./static/dist
-async function cleanJS() {
-  gulp.src(`${currentDir}/static/dist/*.js`, { read: false }).pipe(clean());
-}
+// async function cleanJS() {
+//   gulp.src(`${currentDir}/static/dist/*.js`, { read: false }).pipe(clean());
+// }
 
-var jsFiles = [
-  {
-    module: "lunrjs_gulp",
-    nodePath: "/node_modules/lunr/lunr.js",
-    minimizedFile: "",
-  },
-];
+// var jsFiles = [
+//   {
+//     module: "lunrjs_gulp",
+//     nodePath: "/node_modules/lunr/lunr.js",
+//     minimizedFile: "",
+//   },
+// ];
 
-function minifyJS() {
-  destinationPath = `${currentDir}/static/dist/`;
-  var streams = [];
-  jsFiles.forEach(function (module) {
-    var stream = gulp
-      .src(`${currentDir}/${module["nodePath"]}`)
-      .pipe(hash({ format: "{name}.{hash}.{ext}" }))
-      .pipe(uglify())
-      .pipe(
-        rename(function (path) {
-          // append min to the filename
-          path.basename += "min";
-          // add minimizedFile to the object
-          module["minimizedFile"] = path.basename + path.extname;
-        })
-      )
-      .pipe(gulp.dest(destinationPath));
-    streams.push(stream);
-  });
-  return merge(streams);
-}
+// function minifyJS() {
+//   destinationPath = `${currentDir}/static/dist/`;
+//   var streams = [];
+//   jsFiles.forEach(function (module) {
+//     var stream = gulp
+//       .src(`${currentDir}/${module["nodePath"]}`)
+//       .pipe(hash({ format: "{name}.{hash}.{ext}" }))
+//       .pipe(uglify())
+//       .pipe(
+//         rename(function (path) {
+//           // append min to the filename
+//           path.basename += "min";
+//           // add minimizedFile to the object
+//           module["minimizedFile"] = path.basename + path.extname;
+//         })
+//       )
+//       .pipe(gulp.dest(destinationPath));
+//     streams.push(stream);
+//   });
+//   return merge(streams);
+// }
 
-function insertLunrJS() {
-  var streams = [];
-  jsFiles.forEach(function (module) {
-    // console.log(JSON.stringify(module["minimizedFile"]));
-    var stream = gulp
-      .src([`${themeDir}/layouts/search/single.html`])
-      .pipe(replace(module["module"], module["minimizedFile"]))
-      .pipe(
-        gulp.dest(function (file) {
-          return file.base;
-        })
-      );
-    streams.push(stream);
-  });
-  return merge(streams);
-}
+// function insertLunrJS() {
+//   var streams = [];
+//   jsFiles.forEach(function (module) {
+//     // console.log(JSON.stringify(module["minimizedFile"]));
+//     var stream = gulp
+//       .src([`${themeDir}/layouts/search/single.html`])
+//       .pipe(replace(module["module"], module["minimizedFile"]))
+//       .pipe(
+//         gulp.dest(function (file) {
+//           return file.base;
+//         })
+//       );
+//     streams.push(stream);
+//   });
+//   return merge(streams);
+// }
 
 module.exports = {
   buildTheme: buildTheme,
   buildBlog: gulp.series([
-    buildSearch,
+    // buildSearch,
     buildTheme,
     // gulp.series(cleanJS, minifyJS, insertLunrJS),
     buildHugo,
